140ff4f7aed2ff4ea093f81a39a04e16e123b030
Prodigy00 <simplygiddy@gmail.com>
2024-03-30 18:16:02 +0100

    add middlewares

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,8 +1,11 @@
 package main
 
-import "net/http"
+import (
+	"github.com/justinas/alice"
+	"net/http"
+)
 
-func (app *application) routes() *http.ServeMux {
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux() // equivalent to express.Router()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
@@ -13,5 +16,10 @@ func (app *application) routes() *http.ServeMux {
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
-	return mux
+	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+
+	// Pass the servemux as the 'next' parameter to the secureHeaders middleware.
+	//Because secureHeaders is just a function, and the function returns a
+	// http.Handler we don't need to do anything else.
+	return standard.Then(mux)
 }
